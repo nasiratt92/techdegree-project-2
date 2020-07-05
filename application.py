066610e9d60a7457075ce407dc864449c03b38fd
nasiratt92 <nasiratt92@gmail.com>
2020-07-05 01:55:36 +0100

    first completion attempt

--- application.py
+++ application.py
@@ -1,19 +1,20 @@
 import constants
 import pdb
-
+from pynput.keyboard import Key, Controller
 # dunder_main at the end
 
 # write collection cleaning function
 PLAYERS = constants.PLAYERS
 TEAMS = constants.TEAMS
-
+allocated_teams = []
+allocated_players = []
 
 def clean_data(collection):
-    '''This is function that will take a collection and return a new collection
-    it will clean up all the strings containing number and store them as integers
-    in the new collection.
-    And if the value is Yes/NO it will save the value as true/false in the return
-    collection'''
+    '''This is function that will take a collection and
+    it will clean up the heights key with a value containing number and
+     store them as integer
+    And if the experience value is Yes/NO it will save the value as true/false
+    '''
     for dictionary in collection:
 
         for key, value in dictionary.items():
@@ -28,9 +29,6 @@ def clean_data(collection):
 
 def balance_teams(teams, players):
 
-    allocated_teams = []
-    allocated_players = []
-
     num_players_team = len(players) / len(teams)
 
     for team in teams:
@@ -43,10 +41,41 @@ def balance_teams(teams, players):
             count += 1
         allocated_players.append(list)
 
-    return allocated_players    
+def display_team_print(option, teams, players):
+    option_index = option - 1
+    team_name = teams[option_index].values()
+
+    print(
+        '''
+        Team: {} Stats
+        --------------------
+        Total players: {}
+
+        Players on Team:
+
+        '''.format(team_name, len(players[option_index]) )
+        )
+    for player in players[option_index]:
+        print('* {}'.format(player['name']))
 
 
-clean_data(PLAYERS)
-balance_teams(TEAMS, PLAYERS)
-print(allocated_teams)
-print(len(allocated_players[0]))
+if __name__ == "__main__":
+    balance_teams(TEAMS, PLAYERS)
+    clean_data(PLAYERS)
+    keyboard = Controller()
+    print('BASKETBALL TEAM STATS TOOL')
+    print('\n---- MENU----')
+    print("""
+    Here are your choices:
+    1) Display Team Stats
+    2) Quit
+    """)
+    display_stats = input("Enter an option > ")
+    if display_stats == 1:
+        list_number = 1
+        for team in allocated_teams:
+            print('{}) {}'.format(list_number, team.values()))
+            list_number += 1
+        display_team = input("Enter an option > ")
+        display_team_print(display_team, allocated_teams, allocated_players)
+        print('Press ENTER to continue...')
